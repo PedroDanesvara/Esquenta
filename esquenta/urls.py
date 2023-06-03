d250cbf5b9371f7c834fec6d37b6c849e6597841
samuel agap <agap18@unifesspa.edu.br>
2023-06-02 23:45:37 -0300

    Ajustando tela home.html e inserindo prévi de autenticaçõ de usuários

--- esquenta/urls.py
+++ esquenta/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from coleta.views import listar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('coleta/ofertas', listar )
+    path('coleta/ofertas', listar ),
+    path('auth/', include('usuarios.urls')),
 ]
