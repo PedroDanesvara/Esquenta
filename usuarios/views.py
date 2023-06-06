c34dedce670c564e40fa4e5210e0061bb7fe7471
samuel agap <agap18@unifesspa.edu.br>
2023-06-05 21:29:17 -0300

    arrumando estrutura de arquivos e inserindo validação de usuários

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,17 +6,19 @@ from django.contrib.auth import login as login_django
 
 def cadastro(request):
     if request.method == "GET":
-        return render(request, 'cadastro.html')
+        return render(request, 'login.html')
     else: #Cadastra user
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
 
         user = User.objects.filter(username=username).first()
         if user:
             return HttpResponse("Já existe esse user")
         
-        user = User.objects.create_user(username=username, email=email, password=senha)
+        user = User.objects.create_user(username=username, email=email, password=senha, first_name=nome, last_name=sobrenome)
         return HttpResponse("Cadastrado com sucesso meu pêxe")
 
 def login(request):
