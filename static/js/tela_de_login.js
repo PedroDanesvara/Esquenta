81947b103178f0dbe1267c07679b67d318b82cbf
EduardoRamonL <eduramondevops@gmail.com>
2023-06-08 20:27:27 -0300

    Borda verde do cadastre-se arrumado.
    Animação de deslocamento em login e cadastre-se.

--- static/js/tela_de_login.js
+++ static/js/tela_de_login.js
@@ -1,11 +1,11 @@
 var btnSignin = document.querySelector("#signin");
-var btnSigup = document.querySelector("#sigup");
+var btnSignup = document.querySelector("#sigup");
 var body = document.querySelector("body")
 
 btnSignin.addEventListener("click", function () {
     body.className = "sign-in-js"
 });
 
-btnSigup.addEventListener("click", function () {
+btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js"
 });
